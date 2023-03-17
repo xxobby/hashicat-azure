3c026fbfb36d081e242296322d6d6b27d62240f7
xxobby <xxompletehj@gmail.com>
2023-03-17 14:56:09 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,6 @@ module "network" {
   source  = "app.terraform.io/instruqt-201/network/azurerm"
   version = "5.2.0"
   # insert required variables here
-  resource_group_name="${var.prefix}-workshop"
+  resource_group_name="hj-az-workshop"
+  use_for_each = true
 }
